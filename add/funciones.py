0bfde7e4fa3c96588481732362144aca09913cd1
Leo <leoperez.x@gmail.com>
2023-06-05 22:38:51 -0500

    Iniciando limpieza de datos

--- add/funciones.py
+++
@@ -1,6 +0,0 @@
-def eliminar_nuevaLinea(col):
-    '''
-    Eliminar nueva linea de un string.
-    '''
-    colNombres = col.str.replace('\n', '')
-    return colNombres
--- estudiantes-inscritos-2016.py
+++ estudiantes-inscritos-2016.py
@@ -1,16 +1,19 @@
 import pandas as pd
-import add.funciones as fn
-
 
 # Carga información y salta filas 
 df = pd.read_excel('add/inscritos_2016.xlsx', skiprows=[0,1,2,3,4,5])
 
-# Limpiar nombre de colunmas
+# Nombres de las columnas
 nombresColumnas = df.columns
-nombresColumnasNuevos = fn.eliminar_nuevaLinea(nombresColumnas)
+# Limpiar nombre de colunmas colNombres
+nombresColumnasNuevos = nombresColumnas.str.replace('\n', '')
 # renombrando columnas
 df.columns = nombresColumnasNuevos
 
 # Contar campos vacios por columnas
 print(df.isnull().sum())
 
+# Mostrar filas duplicadas
+filasDuplicadas = df[df.duplicated()]
+print("\nLas filas duplicadas son: ")
+print(filasDuplicadas)
